bcb0ffdc89dcc580bd38044a8684843e6ad87498
Anosh Askari <125911097+anoask@users.noreply.github.com>
2024-10-30 01:44:49 -0400

    Add files via upload

---
+++ Screens/BarcodeScanner/BarcodeScannerView.swift
@@ -0,0 +1,53 @@
+//
+//  BarcodeScannerView.swift
+//  barcodeScanner
+//
+//  Created by Anosh Askari on 9/13/24.
+//
+
+import SwiftUI
+
+
+struct BarcodeScannerView: View {
+    
+    @StateObject var viewModel = BarcodeScannerViewModel()
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                ScannerView(scannedCode: $viewModel.scannedCode,
+                            alertItem: $viewModel.alertItem)
+                    .frame(maxWidth: .infinity, maxHeight: 300)
+                
+                Spacer().frame(height: 60)
+                
+                Label("Scanned Barcode:", systemImage: "barcode.viewfinder")
+                    .font(.title)
+                
+                Text(viewModel.statusText)
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundColor(viewModel.statusTextColor)
+                    .padding()
+            }
+            .navigationTitle("Barcode Scanner")
+            .alert(item: $viewModel.alertItem) { alertItem in
+                Alert(title: Text(alertItem.title),
+                      message: Text(alertItem.message),
+                      dismissButton: alertItem.dismissButton)
+            }
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        BarcodeScannerView()
+    }
+}
+
+
+#Preview {
+    BarcodeScannerView()
+}
---
+++ barcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  barcodeScannerApp.swift
+//  barcodeScanner
+//
+//  Created by Anosh Askari on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct barcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}
